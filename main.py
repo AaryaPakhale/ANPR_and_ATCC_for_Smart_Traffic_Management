1f64a8501584c4d5c326b8e872a7e210da6f4b75
Aarya Pakhale <aaryapakhale01@gmail.com>
2024-12-02 23:52:52 +0530

    added data folder, ocr folder, new models, edited tracker.py

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 from utils.video_utils import read_video, save_video, detect_vehicles
 from object_tracker.tracker import Tracker
 
-# def main():
-#
-#     frames = read_video("data/traffic_3.mp4")
-#
-#     #object tracking
-#     obj_tracker = Tracker()
-#     result = obj_tracker.detect_objects(frames)
-#     output_frames = obj_tracker.draw_annotations(frames, result)
-#
-#
+def main():
+
+    frames = read_video("data/traffic_video.mp4")
+
+    #object tracking
+    obj_tracker = Tracker()
+    result = obj_tracker.detect_objects(frames)
+    output_frames = obj_tracker.draw_annotations(frames, result)
+
+
 #     save_video(output_frames, "output/123.avi")
 
 ###### Main function for 1 image
 
-def main():
+# def main():
 
-    frames = read_video("data/image.jpg")
+#     frames = read_video("data/image.jpg")
 
-    #object tracking
-    obj_tracker = Tracker()
-    result = obj_tracker.detect_frame(frames)
-    print(result)
+#     #object tracking
+#     obj_tracker = Tracker()
+#     result = obj_tracker.detect_frame(frames)
+#     print(result)
 
     # output_frames = obj_tracker.draw_annotations(frames, result)
     #
