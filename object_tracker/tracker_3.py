3a700f98d87367f9383374ed14721f39980a92d3
AaryaPakhale <127590975+AaryaPakhale@users.noreply.github.com>
2024-12-31 16:29:38 +0530

    Update tracker_3.py

--- object_tracker/tracker_3.py
+++ object_tracker/tracker_3.py
@@ -85,7 +85,6 @@
 from ultralytics import YOLO
 import cv2
 
-from sort.sort import *
 import numpy as np
 
 from utils.util import get_car, read_license_plate, write_csv
@@ -100,7 +99,6 @@ class Tracker:
         self.vehicle_detection_model = YOLO("yolov8x.pt")
         self.license_plate_detector = YOLO("number_plate_detection_2nd_dec.pt")
         self.results = {}
-        self.mot_tracker = Sort()
         self.vehicles = [2, 3, 5, 7]
 
     def process_video(self, frames):
@@ -110,16 +108,23 @@ class Tracker:
 
             detections = self.vehicle_detection_model.track(frame,persist=True)[0]
             # print(f"outer detection\n{detections.boxes.data.tolist()}")
+            class_names = detections.names
             detections_ = []
 
-            for detection in detections.boxes.data.tolist():
-                # print(f"internal_detection{detection}")
-                x1, y1, x2, y2, track_id, score, class_id = detection
-                if int(class_id) in self.vehicles:
-                    detections_.append([x1, y1, x2, y2, track_id, score, class_id])
+            # for detection in detections.boxes.data.tolist():
+            #     x1, y1, x2, y2, track_id, score, class_id = detection
+            #     if int(class_id) in self.vehicles:
+            #         detections_.append([x1, y1, x2, y2, track_id, class_id])
 
-            # track_ids = self.mot_tracker.update(np.asarray(detections_))
-            # print(f"track_id: {track_ids}")
+            for detection in detections.boxes:
+                track_id = int(detection.id.tolist()[0])
+                x1,y1,x2,y2 = detection.xyxy.tolist()[0]
+                obj_class_id = detection.cls.tolist()[0]
+                object_class_name = class_names[obj_class_id]
+
+                if int(obj_class_id) in self.vehicles:
+                    detections_.append([x1, y1, x2, y2, track_id, object_class_name])
+
 
             # detect license plates
             license_plates = self.license_plate_detector(frame)[0]
@@ -128,8 +133,7 @@ class Tracker:
 
                 # assign license plate to car
                 # xcar1, ycar1, xcar2, ycar2, car_id = get_car(license_plate, detections_)
-                xcar1, ycar1, xcar2, ycar2, car_id, car_score, car_class = get_car(license_plate, detections_)
-                print(xcar1, ycar1, xcar2, ycar2, car_id, car_score, car_class)
+                xcar1, ycar1, xcar2, ycar2, car_id, car_class = get_car(license_plate, detections_)
 
 
                 if car_id != -1:
@@ -143,30 +147,19 @@ class Tracker:
 
                     license_plate_crop_thresh = 255 - license_plate_crop_thresh
 
-                    object_filename = f"data1/{x1}_{car_id}.jpg"
-                    cv2.imwrite(object_filename, license_plate_crop_thresh)
-
-                    cv2.rectangle(frame, (int(xcar1), int(ycar1)), (int(xcar2), int(ycar2)), (0, 255, 0), 2)
-
-
-
 
                     # read license plate number
                     license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_thresh)
-
                     if license_plate_text is not None:
-                        self.results[frame_no][car_id] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
+                        self.results[frame_no][car_id] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2],
+                                                                  'obj_class':car_class},
                                                       'license_plate': {'bbox': [x1, y1, x2, y2],
                                                                         'text': license_plate_text,
                                                                         'bbox_score': score,
                                                                         'text_score': license_plate_text_score}}
-            frame = cv2.resize(frame, (1280, 720))
-            cv2.imshow('frame', frame)
-            if cv2.waitKey(0) & 0xFF == ord('q'):
-                break
+            # frame = cv2.resize(frame, (1280, 720))
+            # cv2.imshow('frame', frame)
+            # if cv2.waitKey(0) & 0xFF == ord('q'):
+            #     break
         # write results
-        write_csv(self.results, './test1_testing_3_29.csv')
-
-
-
-
+        write_csv(self.results, 'results/main.csv')
